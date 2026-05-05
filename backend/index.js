cf2fc25dc2f511df353e1d354afa4702ad633736
Himanshu <himanshuupadhyay210@gmail.com>
2026-05-05 15:21:19 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,31 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 8080
-const URL_ATLAS = process.env.URL_ATLAS
-const fileUpload = require('express-fileupload')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 8080;
+const URL_ATLAS = process.env.URL_ATLAS;
+const fileUpload = require("express-fileupload");
 
 app.use(
-    cors({
-        origin: [
-            "http://localhost:5173",              // local frontend
-            "https://todo-frontend-vexg.onrender.com" // deployed frontend
-        ],
-        credentials: true,
-    })
+  cors({
+    origin: [
+      "http://localhost:5173", // local frontend
+      "https://to-do-blond-sigma.vercel.app", // deployed frontend
+    ],
+    credentials: true,
+  }),
 );
 
-app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(fileUpload());
 mongoose
-    .connect(URL_ATLAS)
-    .then(() => console.log("DB Connected"))
-    .catch((err) => console.log(err))
+  .connect(URL_ATLAS)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
 
-const userRouter = require('./routes/userRoute')
-app.use('/user', userRouter)
+const userRouter = require("./routes/userRoute");
+app.use("/user", userRouter);
 
-app.listen(port, () => console.log("Server Started at port:-", port))
-
-
+app.listen(port, () => console.log("Server Started at port:-", port));
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,85 +1,49 @@
-const User = require('../model/userModel')
-const jwt = require('jsonwebtoken')
-const secretKey = process.env.SECRET_KEY
+const User = require("../model/userModel");
+const jwt = require("jsonwebtoken");
+const secretKey = process.env.SECRET_KEY;
 
 module.exports = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(" ")[1]
-        if (!token) {
-            return res.status(400).json({ message: "Toekn not found in auth page backend" })
-        }
-        let decode;
-        try {
-            decode = jwt.verify(token, secretKey)
-        } catch (err) {
-            if (err.name === "TokenExpiredError") {
-                return res.status(401).json({
-                    message: "Token expired. Please login again.",
-                    expired: true
-                });
-            }
-        }
-
-        // console.log("lets see whats inside the decode :-", decode);
-        if (!decode) {
-            return res.status(400).json({ message: "Token not decoded in auth page backend" })
-        }
-
-        const existingUser = await User.findOne({ email: decode.email }).select("-password");
-        if (!existingUser) {
-            return res.status(400).json({ message: "User not found in db, auth page backend" })
-        }
-
-        req.user = existingUser
-        next()
-
-
-    } catch (error) {
-        return res.status(500).json("Internal server Error", error)
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      return res
+        .status(400)
+        .json({ message: "Toekn not found in auth page backend" });
     }
-}
+    let decode;
+    try {
+      decode = jwt.verify(token, secretKey);
+    } catch (err) {
+      if (err.name === "TokenExpiredError") {
+        return res.status(401).json({
+          message: "Token expired. Please login again.",
+          expired: true,
+        });
+      }
+    }
 
+    // console.log("lets see whats inside the decode :-", decode);
+    if (!decode) {
+      return res
+        .status(400)
+        .json({ message: "Token not decoded in auth page backend" });
+    }
 
+    const existingUser = await User.findOne({ email: decode.email }).select(
+      "-password",
+    );
+    if (!existingUser) {
+      return res
+        .status(400)
+        .json({ message: "User not found in db, auth page backend" });
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    req.user = existingUser;
+    next();
+  } catch (error) {
+    return res.status(500).json("Internal server Error", error);
+  }
+};
 
 //  const authHeaders = req.headers.authorization
 //         //  const token = req.headers.authorization.split(' ')[1];
